c5de94f31912c636455e2c3a999aa7870c395e55
Marcelo Jorge <marcelojorgea@gmail.com>
2022-06-01 11:37:03 -0300

    add img field to product

--- product/models.py
+++ product/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=7,decimal_places=2)
+    price = models.DecimalField(max_digits=6,decimal_places=2)
     popularity = models.IntegerField()
+    image = models.FileField(upload_to='static/imgs')
+    
